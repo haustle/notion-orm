85dfc107743c29d56ad8b81d6cc17953026c61a9
Vladyslav Yazykov <vladogim97@gmail.com>
2026-03-15 23:28:10 +0100

    Release v0.1.1

--- notion.config.ts
+++
@@ -1,6 +0,0 @@
-const config = {
-  auth: process.env.NOTION_API_KEY ?? "",
-  databaseIds: [process.env.NOTION_TASKS_DB_ID ?? ""],
-};
-
-export default config;
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { tasks } from "../generated/tasks";
-
-export default class NotionORM {
-  public tasks: ReturnType<typeof tasks>;
-
-  constructor(config: { auth: string }) {
-    this.tasks = tasks(config.auth);
-  }
-}
